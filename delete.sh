e72341b57c1bd118a803c72dc7eeb83e1ab5eebc
Mario Mercado <mmercado@classvaluation.com>
2021-07-19 14:23:26 -0500

    adding the deletion of the IAM Service Accounts

--- delete.sh
+++ delete.sh
@@ -4,6 +4,9 @@ read
 kubectl delete -f external-dns/external-dns.yaml
 helm delete --namespace kube-system aws-load-balancer-controller
 
+eksctl delete iamserviceaccount --name external-dns --cluster my-eks-cluster
+eksctl delete iamserviceaccount --name aws-load-balancer-controller --namespace kube-system --cluster my-eks-cluster
+
 eksctl delete cluster --name my-eks-cluster
 
 aws cloudformation delete-stack --stack-name external-dns-iam-policy
