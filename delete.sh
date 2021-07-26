f9323c28a7e841cfbee34b49e99ecffcff183759
Mario Mercado <mmercado@classvaluation.com>
2021-07-26 15:21:43 -0500

    Delete instructions for removing the sample-app Helm releases

--- delete.sh
+++ delete.sh
@@ -1,5 +1,7 @@
-echo "Make sure you have deleted all the helm releases for the sample app"
+echo "Make sure you have deleted all the helm releases for the 'sample app' (ignore aws-load-balancer-controller release, this is deleted automatically by this script)"
 helm ls --all-namespaces
+echo "If all Helm are deleted, please press [ENTER], if not [CTRL+C] and do so"
+echo "If the sample-app releases are not deleted, this won't delete load balancers, target groups, or Route53 records automatically."
 read
 kubectl delete -f external-dns/external-dns.yaml
 helm delete --namespace kube-system aws-load-balancer-controller
